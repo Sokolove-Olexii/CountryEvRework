e4a25e88c78dc3af8c11411931c34627c92daa7a
Sokolove-Olexii <130235909+Sokolove-Olexii@users.noreply.github.com>
2025-05-03 11:29:06 -0400

    some of changes, fixed styles

--- src/js/cards.js
+++ src/js/cards.js
@@ -3,15 +3,19 @@ export function renderCards(e) {
   const ul = document.querySelector('.events');
   const markup = e
     .map(item => {
+      // col-3 mb-4 mx-3
+      // <img src="${item.images[0].url}" class="card-img-top" alt="${item.name}" id="${item.id}">
       return `
-        <li class="card col-3 mb-4 mx-2" >
-          <img src="${item.images[0].url}" class="card-img-top" alt="${item.name}" id="${item.id}">
+        <li class="card1">  
+         <div class="card-img-container" style="background-image: url('${item.images[0].url}')"></div>
+         <div class="pink"></div>
           <div class="card-body">
             <h2 class="card-title">${item.name}</h2>
             <p class="card-text">${item.dates.start.localDate}</p>
-            <p class="card-text">${item._embedded.venues[0].name}</p>
+            <p class="card-text2">${item._embedded.venues[0].name}</p>
           </div>
-        </li>`;
+        </li>
+        `;
     })
     .join('');
   ul.innerHTML = markup;
